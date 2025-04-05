75aaa2aa4f1f8c347ea9aacdb06d5f9beb764261
marder <marcin.derucki@gmail.com>
2025-04-05 15:37:58 +0200

    5.4.2025

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -19,26 +19,34 @@ onMounted(async () => {
 
 <template>
   <section id="table" class="container mx-auto py-10">
-    <table class="min-w-full">
-      <thead class="bg-gray-200 border-b">
-        <tr>
-          <th scope="col" class="w-1/3 text-sm font-medium text-gray-900 px-6 py-4 text-left">Data</th>
-          <th scope="col" class="w-1/3 text-sm font-medium text-gray-900 px-6 py-4 text-left">Temperatura</th>
-          <th scope="col" class="w-1/3 text-sm font-medium text-gray-900 px-6 py-4 text-left">Wilgotność</th>
-          <th scope="col" class="w-1/3 text-sm font-medium text-gray-900 px-6 py-4 text-left">Ciśnienie</th>
-          <th scope="col" class="w-1/3 text-sm font-medium text-gray-900 px-6 py-4 text-left">Opis</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr class="even:bg-gray-100 odd:bg-white border-b" v-for="d in lastData" :key="d._id">
-          <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{{
-            dayjs(d.readingDate).format('YYYY-MM-DD, HH:mm:ss') }}</td>
-          <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">{{ d.temperature }} °C</td>
-          <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">{{ d.humidity }}%</td>
-          <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">{{ d.pressure }} hPa</td>
-          <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">{{ d.description }}</td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="flex flex-col">
+      <div class="overflow-x-auto shadow-md sm:rounded-lg">
+        <div class="inline-block min-w-full align-middle">
+          <div class="overflow-hidden">
+            <table class="min-w-full divide-y divide-gray-200 table-fixed dark:divide-gray-700">
+              <thead class="bg-gray-200 border-b">
+                <tr>
+                  <th scope="col" class="w-1/3 text-sm font-medium text-gray-900 px-4 py-2 text-left">Data</th>
+                  <th scope="col" class="w-1/3 text-sm font-medium text-gray-900 px-4 py-2 text-left">Temperatura</th>
+                  <th scope="col" class="w-1/3 text-sm font-medium text-gray-900 px-4 py-2 text-left">Wilgotność</th>
+                  <th scope="col" class="w-1/3 text-sm font-medium text-gray-900 px-4 py-2 text-left">Ciśnienie</th>
+                  <th scope="col" class="w-1/3 text-sm font-medium text-gray-900 px-4 py-2 text-left">Opis</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr class="even:bg-gray-100 odd:bg-white border-b" v-for="d in lastData" :key="d._id">
+                  <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{{
+                    dayjs(d.readingDate).format('YYYY-MM-DD, HH:mm:ss') }}</td>
+                  <td class="text-sm text-gray-900 font-light px-4 py-1 whitespace-nowrap">{{ d.temperature }} °C</td>
+                  <td class="text-sm text-gray-900 font-light px-4 py-1 whitespace-nowrap">{{ d.humidity }}%</td>
+                  <td class="text-sm text-gray-900 font-light px-4 py-1 whitespace-nowrap">{{ d.pressure }} hPa</td>
+                  <td class="text-sm text-gray-900 font-light px-4 py-1 whitespace-nowrap">{{ d.description }}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
