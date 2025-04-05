1dfb209223a4efd9c0eb5c8f62dcf3a50162fe44
marder <marcin.derucki@gmail.com>
2025-04-05 14:45:15 +0200

    5.4.2025

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -33,9 +33,9 @@ onMounted(async () => {
         <tr class="even:bg-gray-100 odd:bg-white border-b" v-for="d in lastData" :key="d._id">
           <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{{
             dayjs(d.readingDate).format('YYYY-MM-DD, HH:mm:ss') }}</td>
-          <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">{{ d.temperature }}</td>
-          <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">{{ d.humidity }}</td>
-          <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">{{ d.pressure }}</td>
+          <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">{{ d.temperature }} °C</td>
+          <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">{{ d.humidity }}%</td>
+          <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">{{ d.pressure }} hPa</td>
           <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">{{ d.description }}</td>
         </tr>
       </tbody>
